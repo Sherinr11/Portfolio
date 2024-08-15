31b9d42051ee4d155cd1d47a8ba9372aa9b54507
Sherin Rose <119114121+Sherinr11@users.noreply.github.com>
2024-08-15 21:16:27 +0530

    Add files via upload

---
+++ portfolio/assets/js/animate.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const animatedText = document.getElementById('animated-text');
+    const texts = [ "I'm a Data Science Enthusiast"];
+    let index = 0;
+    
+    function animateText() {
+     
+        animatedText.textContent = texts[index];
+        
+        
+        index = (index + 1) % texts.length;
+        
+       
+        animatedText.classList.remove('anim-typewriter-end');
+        void animatedText.offsetWidth; 
+        animatedText.classList.add('anim-typewriter');
+        
+        setTimeout(() => {
+            animatedText.classList.remove('anim-typewriter');
+            animatedText.classList.add('anim-typewriter-end');
+        }, 8000); 
+        
+     
+        setTimeout(() => {
+            animatedText.classList.remove('anim-typewriter-end');
+            animatedText.classList.add('anim-typewriter');
+        
+        
+            setTimeout(() => {
+                animatedText.classList.remove('anim-typewriter');
+                animatedText.classList.add('anim-typewriter-end');
+            }, 8000); 
+        }, 10); 
+
+        setTimeout(animateText, 10000); 
+    }
+
+    animateText();
+});
